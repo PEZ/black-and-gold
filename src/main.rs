0f57e0c373b67d8d1a9ba139765e2ffb1a05f6d7
Peter Strömberg <pez@pezius.com>
2023-12-27 18:28:45 +0100

    Square + Circle Collision

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,11 @@ struct Shape {
 }
 
 impl Shape {
-    fn collides_with(&self, other: &Self) -> bool {
-        self.rect().overlaps(&other.rect())
-    }
-
-    fn rect(&self) -> Rect {
-        Rect {
-            x: self.x - self.size / 2.0,
-            y: self.y - self.size / 2.0,
-            w: self.size,
-            h: self.size,
-        }
+    fn collides_with_circle(&self, circle: &Shape) -> bool {
+        let half = self.size / 2.0;
+        let dx = (self.x - circle.x).abs().max(half) - half;
+        let dy = (self.y - circle.y).abs().max(half) - half;
+        dx * dx + dy * dy <= circle.size * circle.size / 4.0
     }
 }
 
@@ -34,7 +28,7 @@ async fn main() {
 
     let mut squares = vec![];
     let mut circle = Shape {
-        size: BALL_RADIUS,
+        size: BALL_RADIUS * 2.0,
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
@@ -93,7 +87,10 @@ async fn main() {
                 square.y += square.speed * delta_time;
             }
 
-            if squares.iter().any(|square| circle.collides_with(square)) {
+            if squares
+                .iter()
+                .any(|square| square.collides_with_circle(&circle))
+            {
                 game_over = true;
             }
 
@@ -110,7 +107,7 @@ async fn main() {
             );
         }
 
-        draw_circle(circle.x, circle.y, circle.size, circle.color);
+        draw_circle(circle.x, circle.y, circle.size / 2.0, circle.color);
 
         if game_over {
             let game_over_text = "GAME OVER! Press space to restart.";
