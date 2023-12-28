d9a37416c54d90a99af417f81b72e89cea802797
Peter Strömberg <pez@pezius.com>
2023-12-28 13:38:21 +0100

    Add GameState

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
 
-use macroquad::prelude::*;
 use macroquad::rand::ChooseRandom;
+use macroquad::prelude::*;
 use std::f32::consts::PI;
 use std::fs;
 
@@ -18,7 +18,7 @@ fn oscillating_alpha(base_color: Color, cycles_per_second: f32) -> Color {
 lazy_static! {
     static ref COLORS: Vec<Color> = vec![
         BEIGE, BLACK, BLUE, BROWN, DARKBLUE, DARKBROWN, DARKGRAY, DARKGREEN, DARKPURPLE, GRAY,
-        GREEN, LIME, MAGENTA, MAROON, ORANGE, PINK, PURPLE, RED, SKYBLUE, VIOLET, WHITE, YELLOW,
+        GREEN, LIME, MAGENTA, MAROON, ORANGE, PINK, PURPLE, RED, SKYBLUE, VIOLET, BLACK, YELLOW,
     ];
 }
 
@@ -52,6 +52,77 @@ impl Shape {
     }
 }
 
+enum GameState {
+    MainMenu,
+    Playing,
+    Paused,
+    GameOver,
+}
+
+fn draw_game_objects(
+    squares: &[Shape],
+    bullets: &[Shape],
+    circle: &Shape,
+    score: u32,
+    high_score: u32,
+    high_score_beaten: bool,
+) {
+    for square in squares {
+        draw_rectangle(
+            square.x - square.size / 2.0,
+            square.y - square.size / 2.0,
+            square.size,
+            square.size,
+            square.color,
+        );
+    }
+
+    for bullet in bullets {
+        draw_rectangle(
+            bullet.x - bullet.size / 2.0,
+            bullet.y - bullet.size / 2.0,
+            bullet.size,
+            bullet.size,
+            bullet.color,
+        );
+    }
+    draw_circle(circle.x, circle.y, circle.size / 2.0, circle.color);
+
+    draw_text(
+        format!("Score: {}", score).as_str(),
+        10.0,
+        35.0,
+        25.0,
+        BLACK,
+    );
+    let high_score_text = format!("High score: {}", high_score);
+    let high_score_beaten_text = if high_score_beaten {
+        "New high score!"
+    } else {
+        ""
+    };
+
+    let text_dimensions = measure_text(high_score_text.as_str(), None, 25, 1.0);
+    draw_text(
+        high_score_text.as_str(),
+        screen_width() - text_dimensions.width - 10.0,
+        35.0,
+        25.0,
+        BLACK,
+    );
+
+    if high_score_beaten {
+        let text_dimensions = measure_text(high_score_beaten_text, None, 25, 1.0);
+        draw_text(
+            high_score_beaten_text,
+            screen_width() - text_dimensions.width - 10.0,
+            35.0 + text_dimensions.height + text_dimensions.offset_y,
+            25.0,
+            oscillating_alpha(BLACK, 3.0),
+        );
+    }
+}
+
 #[macroquad::main("My first Macroquad game")]
 async fn main() {
     rand::srand(miniquad::date::now() as u64);
@@ -74,172 +145,156 @@ async fn main() {
         collided: false,
     };
 
-    let mut game_over = false;
+    let mut game_state = GameState::MainMenu;
 
     loop {
         clear_background(GOLD);
-        let delta_time = get_frame_time();
-        let movement = delta_time * MOVEMENT_SPEED;
 
-        if game_over && is_key_pressed(KeyCode::Space) {
-            squares.clear();
-            bullets.clear();
-            circle.x = screen_width() / 2.0;
-            circle.y = screen_height() / 2.0;
-            score = 0;
-            game_over = false;
-            high_score_beaten = false;
-        }
-
-        if !game_over {
-            if is_key_down(KeyCode::Right) {
-                circle.x += movement;
-            }
-            if is_key_down(KeyCode::Left) {
-                circle.x -= movement;
-            }
-            if is_key_down(KeyCode::Down) {
-                circle.y += movement;
-            }
-            if is_key_down(KeyCode::Up) {
-                circle.y -= movement;
-            }
-
-            circle.x = circle
-                .x
-                .min(screen_width() - BALL_RADIUS)
-                .max(0.0 + BALL_RADIUS);
-            circle.y = circle
-                .y
-                .min(screen_height() - BALL_RADIUS)
-                .max(0.0 + BALL_RADIUS);
-
-            if is_key_pressed(KeyCode::Space)
-                && get_time() - last_bullet_time > 1.0 / MAX_BULLETS_PER_SECOND
-            {
-                last_bullet_time = get_time();
-                bullets.push(Shape {
-                    x: circle.x,
-                    y: circle.y,
-                    speed: circle.speed * 2.0,
-                    color: MAROON,
-                    size: 5.0,
-                    collided: false,
-                });
-            }
-
-            if rand::gen_range(0, 99) >= 95 {
-                let size = rand::gen_range(16.0, 64.0);
-                squares.push(Shape {
-                    size,
-                    speed: rand::gen_range(50.0, 150.0),
-                    x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
-                    y: -size,
-                    color: *COLORS.choose().unwrap(),
-                    collided: false,
-                });
-            }
-
-            for square in &mut squares {
-                square.y += square.speed * delta_time;
-            }
-            for bullet in &mut bullets {
-                bullet.y -= bullet.speed * delta_time;
-            }
-
-            if squares
-                .iter()
-                .any(|square| square.collides_with_circle(&circle))
-            {
-                if score == high_score {
-                    fs::write("highscore.dat", high_score.to_string()).ok();
+        match game_state {
+            GameState::MainMenu => {
+                if is_key_pressed(KeyCode::Escape) {
+                    break;
                 }
-                game_over = true;
-            }
+                if is_key_pressed(KeyCode::Space) {
+                    squares.clear();
+                    bullets.clear();
+                    circle.x = screen_width() / 2.0;
+                    circle.y = screen_height() / 2.0;
+                    score = 0;
+                    high_score_beaten = false;
+                    game_state = GameState::Playing;
+                }
+                let text = "Press space to start.";
+                let text_dimensions = measure_text(text, None, 32, 1.0);
 
-            for square in squares.iter_mut() {
-                for bullet in bullets.iter_mut() {
-                    if bullet.collides_with(square) {
-                        bullet.collided = true;
-                        square.collided = true;
-                        score += square.size.round() as u32;
-                        if score > high_score {
-                            high_score_beaten = true;
-                            high_score = score;
+                let text_x = (screen_width() - text_dimensions.width) / 2.0;
+                let text_y =
+                    screen_height() / 2.0 - text_dimensions.offset_y + text_dimensions.height;
+
+                draw_text(text, text_x, text_y, 32.0, BLACK);
+            }
+            GameState::Playing => {
+                if is_key_pressed(KeyCode::Escape) {
+                    game_state = GameState::Paused;
+                }
+                let delta_time = get_frame_time();
+                let movement = delta_time * MOVEMENT_SPEED;
+
+                if is_key_down(KeyCode::Right) {
+                    circle.x += movement;
+                }
+                if is_key_down(KeyCode::Left) {
+                    circle.x -= movement;
+                }
+                if is_key_down(KeyCode::Down) {
+                    circle.y += movement;
+                }
+                if is_key_down(KeyCode::Up) {
+                    circle.y -= movement;
+                }
+
+                circle.x = circle
+                    .x
+                    .min(screen_width() - BALL_RADIUS)
+                    .max(0.0 + BALL_RADIUS);
+                circle.y = circle
+                    .y
+                    .min(screen_height() - BALL_RADIUS)
+                    .max(0.0 + BALL_RADIUS);
+
+                if is_key_pressed(KeyCode::Space)
+                    && get_time() - last_bullet_time > 1.0 / MAX_BULLETS_PER_SECOND
+                {
+                    last_bullet_time = get_time();
+                    bullets.push(Shape {
+                        x: circle.x,
+                        y: circle.y,
+                        speed: circle.speed * 2.0,
+                        color: MAROON,
+                        size: 5.0,
+                        collided: false,
+                    });
+                }
+
+                if rand::gen_range(0, 99) >= 95 {
+                    let size = rand::gen_range(16.0, 64.0);
+                    squares.push(Shape {
+                        size,
+                        speed: rand::gen_range(50.0, 150.0),
+                        x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                        y: -size,
+                        color: *COLORS.choose().unwrap(),
+                        collided: false,
+                    });
+                }
+
+                for square in &mut squares {
+                    square.y += square.speed * delta_time;
+                }
+                for bullet in &mut bullets {
+                    bullet.y -= bullet.speed * delta_time;
+                }
+
+                if squares
+                    .iter()
+                    .any(|square| square.collides_with_circle(&circle))
+                {
+                    if score == high_score {
+                        fs::write("highscore.dat", high_score.to_string()).ok();
+                    }
+                    game_state = GameState::GameOver;
+                }
+
+                for square in squares.iter_mut() {
+                    for bullet in bullets.iter_mut() {
+                        if bullet.collides_with(square) {
+                            bullet.collided = true;
+                            square.collided = true;
+                            score += square.size.round() as u32;
+                            if score > high_score {
+                                high_score_beaten = true;
+                                high_score = score;
+                            }
                         }
                     }
                 }
+
+                squares.retain(|square| square.y < screen_width() + square.size);
+                bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
+                squares.retain(|square| !square.collided);
+                bullets.retain(|bullet| !bullet.collided);
+
+                draw_game_objects(&squares, &bullets, &circle, score, high_score, high_score_beaten);
             }
+            GameState::Paused => {
+                if is_key_pressed(KeyCode::Space) {
+                    game_state = GameState::Playing;
+                }
+                draw_game_objects(&squares, &bullets, &circle, score, high_score, high_score_beaten);
+                let text = "Paused";
+                let text_dimensions = measure_text(text, None, 32, 1.0);
+                draw_text(
+                    text,
+                    screen_width() / 2.0 - text_dimensions.width / 2.0,
+                    screen_height() / 2.0,
+                    32.0,
+                    BLACK,
+                );
+            }
+            GameState::GameOver => {
+                if is_key_pressed(KeyCode::Space) || is_key_pressed(KeyCode::Escape) {
+                    game_state = GameState::MainMenu;
+                }
+                draw_game_objects(&squares, &bullets, &circle, score, high_score, high_score_beaten);
+                let game_over_text = "GAME OVER!";
+                let text_dimensions = measure_text(game_over_text, None, 32, 1.0);
 
-            squares.retain(|square| square.y < screen_width() + square.size);
-            bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
-            squares.retain(|square| !square.collided);
-            bullets.retain(|bullet| !bullet.collided);
-        }
+                let text_x = (screen_width() - text_dimensions.width) / 2.0;
+                let text_y =
+                    screen_height() / 2.0 - text_dimensions.offset_y + text_dimensions.height;
 
-        for square in &squares {
-            draw_rectangle(
-                square.x - square.size / 2.0,
-                square.y - square.size / 2.0,
-                square.size,
-                square.size,
-                square.color,
-            );
-        }
-
-        for bullet in &bullets {
-            draw_rectangle(
-                bullet.x - bullet.size / 2.0,
-                bullet.y - bullet.size / 2.0,
-                bullet.size,
-                bullet.size,
-                bullet.color,
-            );
-        }
-        draw_circle(circle.x, circle.y, circle.size / 2.0, circle.color);
-
-        draw_text(
-            format!("Poäng: {}", score).as_str(),
-            10.0,
-            35.0,
-            25.0,
-            WHITE,
-        );
-        let high_score_text = format!("High score: {}", high_score);
-        let high_score_beaten_text = if high_score_beaten {
-            "New high score!"
-        } else {
-            ""
-        };
-
-        let text_dimensions = measure_text(high_score_text.as_str(), None, 25, 1.0);
-        draw_text(
-            high_score_text.as_str(),
-            screen_width() - text_dimensions.width - 10.0,
-            35.0,
-            25.0,
-            WHITE,
-        );
-
-        if high_score_beaten {
-            let text_dimensions = measure_text(high_score_beaten_text, None, 25, 1.0);
-            draw_text(
-                high_score_beaten_text,
-                screen_width() - text_dimensions.width - 10.0,
-                35.0 + text_dimensions.height + text_dimensions.offset_y,
-                25.0,
-                oscillating_alpha(WHITE, 3.0),
-            );
-        }
-
-        if game_over {
-            let game_over_text = "GAME OVER! Press space to restart.";
-            let text_dimensions = measure_text(game_over_text, None, 32, 1.0);
-
-            let text_x = (screen_width() - text_dimensions.width) / 2.0;
-            let text_y = screen_height() / 2.0 - text_dimensions.offset_y + text_dimensions.height;
-
-            draw_text(game_over_text, text_x, text_y, 32.0, WHITE);
+                draw_text(game_over_text, text_x, text_y, 32.0, BLACK);
+            }
         }
 
         next_frame().await
