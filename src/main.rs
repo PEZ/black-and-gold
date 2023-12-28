585e6e5d018bf07211b328174fdec6faaa4b65fc
Peter Strömberg <pez@pezius.com>
2023-12-28 17:50:02 +0100

    Try without unused attributes

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ const FRAGMENT_SHADER: &str = include_str!("starfield-shader.glsl");
 
 const VERTEX_SHADER: &str = "#version 100
 attribute vec3 position;
-attribute vec2 texcoord;
-attribute vec4 color0;
+// attribute vec2 texcoord;
+// attribute vec4 color0;
 varying float iTime;
 
 uniform mat4 Model;
