56938ee98d382400193d7b9bdd89f210bfd0650c
Peter Strömberg <pez@pezius.com>
2024-01-04 17:10:58 +0100

    Add script for building to device

---
+++ scripts/ios-build-to-device.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <signing-identity> <device-id>"
+    exit 1
+fi
+SIGNING_IDENTITY=$1
+DEVICE_ID=$2
+
+BINARY_NAME="mitt-spel"
+APP_NAME="MittSpel.app"
+
+cargo build --target aarch64-apple-ios --release
+cp -r assets "ios/$APP_NAME"
+cp target/aarch64-apple-ios/release/$BINARY_NAME "ios/$APP_NAME/"
+codesign --force --timestamp=none --sign "$SIGNING_IDENTITY" --entitlements ios/entitlements.xml "ios/$APP_NAME"
+ios-deploy -i "$DEVICE_ID" -b "ios/$APP_NAME"
