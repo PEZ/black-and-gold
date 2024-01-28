780d1f229985094a267ee0007d8b99c3730a7ccd
Peter Strömberg <pez@pezius.com>
2024-01-28 15:08:05 +0100

    Draw balls

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod ios;
 
 use std::f32::consts::PI;
 
-use macroquad::audio::{load_sound, play_sound, play_sound_once, set_sound_volume, Sound, PlaySoundParams};
+use macroquad::audio::{
+    load_sound, play_sound, play_sound_once, set_sound_volume, PlaySoundParams, Sound,
+};
 
 use macroquad::experimental::animation::{AnimatedSprite, Animation};
 use macroquad::experimental::collections::storage;
@@ -15,51 +17,50 @@ mod simple_logger;
 const GAME_TITLE: &str = "Black & Gold";
 const MOVEMENT_SPEED: f32 = 400.0;
 const BOARD_TILES_X: usize = 10;
-const BOARD_TILES_Y: usize = 10;
 
 struct Resources {
-  pub theme_music: Sound,
-  pub sound_explosion: Sound,
-  pub sound_laser: Sound,
+    pub theme_music: Sound,
+    pub sound_explosion: Sound,
+    pub sound_laser: Sound,
 }
 
 impl Resources {
-  async fn new() -> Result<Resources, macroquad::Error> {
-      let theme_music = load_sound("8bit-spaceshooter.wav").await?;
-      let sound_explosion = load_sound("explosion.wav").await?;
-      let sound_laser = load_sound("laser.wav").await?;
+    async fn new() -> Result<Resources, macroquad::Error> {
+        let theme_music = load_sound("8bit-spaceshooter.wav").await?;
+        let sound_explosion = load_sound("explosion.wav").await?;
+        let sound_laser = load_sound("laser.wav").await?;
 
-      Ok(Resources {
-          theme_music,
-          sound_explosion,
-          sound_laser,
-      })
-  }
+        Ok(Resources {
+            theme_music,
+            sound_explosion,
+            sound_laser,
+        })
+    }
 
-  pub async fn load() -> Result<(), macroquad::Error> {
-      let resources_loading = start_coroutine(async move {
-          let resources = Resources::new().await.unwrap();
-          storage::store(resources);
-      });
+    pub async fn load() -> Result<(), macroquad::Error> {
+        let resources_loading = start_coroutine(async move {
+            let resources = Resources::new().await.unwrap();
+            storage::store(resources);
+        });
 
-      while !resources_loading.is_done() {
-          clear_background(BLACK);
-          let text = format!(
-              "Loading resources {}",
-              ".".repeat(((get_time() * 2.) as usize) % 4)
-          );
-          draw_text(
-              &text,
-              screen_width() / 2. - 160.,
-              screen_height() / 2.,
-              40.,
-              WHITE,
-          );
-          next_frame().await;
-      }
+        while !resources_loading.is_done() {
+            clear_background(BLACK);
+            let text = format!(
+                "Loading resources {}",
+                ".".repeat(((get_time() * 2.) as usize) % 4)
+            );
+            draw_text(
+                &text,
+                screen_width() / 2. - 160.,
+                screen_height() / 2.,
+                40.,
+                WHITE,
+            );
+            next_frame().await;
+        }
 
-      Ok(())
-  }
+        Ok(())
+    }
 }
 
 fn oscillating_alpha(base_color: Color, cycles_per_second: f32) -> Color {
@@ -76,31 +77,52 @@ fn draw_game_title() {
         screen_width() / 2.0 - text_dimensions.width / 2.0,
         screen_height() / 4.0,
         font_size as f32,
-        oscillating_alpha(GOLD, 3.0)
+        oscillating_alpha(GOLD, 3.0),
     );
 }
 
 struct Ball {
     size: f32,
+    direction: f32,
     speed: f32,
     x: f32,
     y: f32,
     color: Color,
     collided: bool,
 }
-  
+
 impl Ball {
+    fn new(color: Color) -> Self {
+        let directions = vec![
+            std::f32::consts::FRAC_PI_4,
+            std::f32::consts::FRAC_PI_4 * 3.0,
+            std::f32::consts::FRAC_PI_4 * 5.0,
+            std::f32::consts::FRAC_PI_4 * 7.0,
+        ];
+        let direction = *rand::ChooseRandom::choose(&directions).unwrap();
+
+        Self {
+            size: 10.0,
+            direction,
+            speed: 5.0,
+            x: 0.0,
+            y: 0.0,
+            color,
+            collided: false,
+        }
+    }
+
     pub fn collides_with_circle(&self, circle: &Ball) -> bool {
         let half = self.size / 2.0;
         let dx = (self.x - circle.x).abs().max(half) - half;
         let dy = (self.y - circle.y).abs().max(half) - half;
         dx * dx + dy * dy <= circle.size * circle.size / 4.0
     }
-  
+
     pub fn collides_with(&self, other: &Self) -> bool {
         self.rect().overlaps(&other.rect())
     }
-  
+
     fn rect(&self) -> Rect {
         Rect {
             x: self.x - self.size / 2.0,
@@ -118,17 +140,38 @@ struct Board {
     y: f32,
 }
 
-fn draw_board(
-    board: &Board,
-    black: &Ball,
-    gold: &Ball,
-) {
+impl Board {
+    fn new() -> Self {
+        let mut tiles: Vec<Vec<bool>> = (0..BOARD_TILES_X - 1)
+            .map(|i| {
+                (0..BOARD_TILES_X)
+                    .map(|j| j <= BOARD_TILES_X - 2 - i)
+                    .collect()
+            })
+            .collect();
+
+        let screen_width = screen_width();
+        let screen_height = screen_height();
+
+        let size = f32::min(screen_width, screen_height);
+        let x = screen_width / 2.0 - size / 2.0;
+        let y = screen_height / 2.0 - size / 2.0;
+
+        Self { tiles, size, x, y }
+    }
+}
+
+pub fn draw_circle_100(x: f32, y: f32, r: f32, color: Color) {
+    draw_poly(x, y, 100, r, 0., color);
+}
+
+fn draw_board(board: &Board, black: &Ball, gold: &Ball) {
     let tile_size = board.size / board.tiles.len() as f32;
 
-    for (i, row) in board.tiles.iter().enumerate() {
-        for (j, &tile) in row.iter().enumerate() {
-            let x = board.x + j as f32 * tile_size;
-            let y = board.y + i as f32 * tile_size;
+    for (xi, row) in board.tiles.iter().enumerate() {
+        for (yi, &tile) in row.iter().enumerate() {
+            let x = board.x + yi as f32 * tile_size;
+            let y = board.y + xi as f32 * tile_size;
 
             if tile {
                 draw_rectangle(x, y, tile_size, tile_size, gold.color);
@@ -137,14 +180,19 @@ fn draw_board(
             }
         }
     }
+    draw_circle_100(gold.x, gold.y, gold.size / 2.0, gold.color);
+    draw_circle_100(black.x, black.y, black.size / 2.0, black.color);
 }
-    
+
 enum GameState {
     Starting,
     Playing,
 }
 
-#[macroquad::main("Black and Gold")]
+#[macroquad::main("Black and Gold", Conf {
+    sample_count: 4,
+    ..Default::default()
+})]
 async fn main() -> Result<(), macroquad::Error> {
     rand::srand(miniquad::date::now() as u64);
 
@@ -165,35 +213,11 @@ async fn main() -> Result<(), macroquad::Error> {
         },
     );
 
-    let mut gold = Ball {
-        size: 0.0,
-        speed: 0.0,
-        x: 0.0,
-        y: 0.0,
-        color: GOLD,
-        collided: false,
-    };
-    
-    let mut black = Ball {
-        size: 0.0,
-        speed: 0.0,
-        x: 0.0,
-        y: 0.0,
-        color: BLACK,
-        collided: false,
-    };
-    
-    let mut tiles: Vec<Vec<bool>> = (0..BOARD_TILES_Y).map(|i| {
-        (0..BOARD_TILES_X).map(|j| j <= BOARD_TILES_Y - 1 - i).collect()
-    }).collect();
-    
-    let mut board = Board {
-        tiles,
-        size: 0.0,
-        x: 0.0,
-        y: 0.0,
-    };
-    
+    let mut gold = Ball::new(GOLD);
+    let mut black = Ball::new(BLACK);
+
+    let mut board = Board::new();
+
     let mut game_state = GameState::Playing;
 
     loop {
@@ -206,23 +230,26 @@ async fn main() -> Result<(), macroquad::Error> {
         board.x = screen_width / 2.0 - board.size / 2.0;
         board.y = screen_height / 2.0 - board.size / 2.0;
 
-        let tile_size: f32 = screen_width / BOARD_TILES_X as f32;
+        let tile_size: f32 = board.size / BOARD_TILES_X as f32;
 
         gold.size = tile_size;
         black.size = tile_size;
-                
+
+        gold.x = board.x + board.size * 3.0 / 4.0 - gold.size / 2.0;
+        gold.y = board.y + board.size * 3.0 / 4.0 - gold.size / 2.0;
+
+        black.x = board.x + board.size / 4.0 - black.size / 2.0;
+        black.y = board.y + board.size / 4.0 - black.size / 2.0;
+
         match game_state {
             GameState::Starting => {
                 draw_game_title();
             }
             GameState::Playing => {
-                draw_board(
-                    &board,
-                    &black,
-                    &gold,
-                );
+                draw_board(&board, &black, &gold);
             }
         }
+
         next_frame().await
     }
 }
